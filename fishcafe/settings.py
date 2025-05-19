685f619485e608d67c85405106b02a32659ea8f9
ndagirelillian <ndagirelillian15@gmail.com>
2025-05-19 23:07:26 +0300

    hosts

--- fishcafe/settings.py
+++ fishcafe/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-uxkz-bvpq9boe&d611*w9o5vg1$bvtor-)y1@ur*ho52ocx(pt
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '147.182.132.190']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
