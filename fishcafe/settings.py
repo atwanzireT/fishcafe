0b3cb1c2f75590c2b8dc13633830e9355705408e
ndagirelillian <ndagirelillian15@gmail.com>
2025-05-19 12:30:51 +0300

    settings modification

--- fishcafe/settings.py
+++ fishcafe/settings.py
@@ -94,24 +94,26 @@ WSGI_APPLICATION = 'fishcafe.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fishcafe',
+        'USER': 'fedora',
+        'PASSWORD': 'atwanzire',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'digitalchronicle_fishcafeDB',
-#         'USER': 'digitalchronicle_atwanzire',
-#         'PASSWORD': 'Bamwite2broz',
-#         'HOST': 'localhost',
-#         'PORT': '3306',
-#     }
-# }
 
 
 # Password validation
@@ -147,15 +149,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-STATIC_URL = '/static/'
-STATIC_ROOT = "home/digitalchronicle/public_html/static"
-STATICFILES_DIRS = [
-    BASE_DIR, 'static'
-]
+STATIC_URL = 'static/'
+# STATICFILES_DIRS =['static/']
+STATIC_ROOT = BASE_DIR/'static'
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = "home2/digitalchronicle/public_html/fishcafeandlounge.com/media"
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_URL = 'media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_ROOT = BASE_DIR/'media'
+
+
 
 # Login/Logout URLs
 LOGIN_REDIRECT_URL = '/manager/'
