a13de655ddefcd23fdac52febdaf2670614f64e1
ndagirelillian <https://github.com/ndagirelillian>
2025-12-15 11:13:17 +0300

    d= True

--- fishcafe/settings.py
+++ fishcafe/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uxkz-bvpq9boe&d611*w9o5vg1$bvtor-)y1@ur*ho52ocx(pt'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
 '*'
